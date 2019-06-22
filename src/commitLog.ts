c125782980c8452cf6b1252fc9642acb6b486e36
Ibrahim <8332931+ibrahimbutt@users.noreply.github.com>
2019-06-22 17:25:21 +0100

    feat: save commit log to file

--- src/commitLog.ts
+++ src/commitLog.ts
@@ -5,12 +5,29 @@ export default class CommitLog {
   private commits: Array<Commit>;
 
   constructor() {
-    const logBuffer: Buffer = execSync(`git log --pretty=format:"%s %h"`);
-    const logString: String = logBuffer.toString();
-    this.commits = logString.split("\n").map(commit => new Commit(commit));
+    const log: String = this.getGitLog();
+    this.commits = this.parseCommits(log);
   }
 
   public getCommits(): Array<Commit> {
     return this.commits;
+  }
+
+  private getGitLog(): String {
+    const logBuffer: Buffer = this.execSync();
+    return logBuffer.toString();
+  }
+
+  private execSync(): Buffer {
+    return execSync(`git log --pretty=format:"%s %h"`);
+  }
+
+  private parseCommits(log: String): Array<Commit> {
+    const commits = log.split("\n");
+    return commits.map(this.createCommit);
+  }
+
+  private createCommit(commit: String): Commit {
+    return new Commit(commit);
   }
 }
