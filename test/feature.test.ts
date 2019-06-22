14aaad015dff0c0b5bb0379c2a5a2749b058636d
Ibrahim Butt <ibrahimbutt1993@gmail.com>
2019-06-22 16:31:22 +0100

    feat: commits are saved to file

--- test/feature.test.ts
+++ test/feature.test.ts
@@ -1,8 +1,12 @@
 import CommitLog from "../src/commitLog";
+import FileSystem from "../src/fileSystem";
 
 import ActualCommitFormatter from "./lib/actualCommitFormatter";
 import StubExecSyncGitLog from "./lib/stubExecSyncGitLog";
+import { conditionalExpression } from "@babel/types";
 const testData = require("./lib/data");
+
+const fs = require("fs");
 
 describe("Feature Tests", () => {
   test("User can retrieve commit log", () => {
@@ -20,5 +24,25 @@ describe("Feature Tests", () => {
 
     expect(actual.first).toEqual(expected.first);
     expect(actual.second).toEqual(expected.second);
+  });
+
+  test("User can save commits to a file", () => {
+    const stubbedCommits = {
+      first: testData.commits.first,
+      second: testData.commits.second
+    };
+
+    StubExecSyncGitLog.stubCommits(stubbedCommits);
+
+    const commitLog = new CommitLog();
+    const commits = commitLog.getCommits();
+
+    let content = "";
+    commits.forEach(commit => (content += commit.getDetails() + "\n"));
+
+    fs.writeFileSync = jest.fn();
+    FileSystem.create("CHANGELOG.md", content);
+
+    expect(fs.writeFileSync).toHaveBeenCalledWith("CHANGELOG.md", content);
   });
 });
