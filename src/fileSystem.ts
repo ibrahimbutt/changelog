6befe81d7bcbc407c1bb9f5768264c849588f01e
Ibrahim <8332931+ibrahimbutt@users.noreply.github.com>
2019-06-23 12:14:20 +0100

    Revert "fix: properly categorise commits that have substrings of commit types"
    This reverts commit 0417adca522df297c301047b57470279becafd46.

--- src/fileSystem.ts
+++
@@ -1,8 +0,0 @@
-const fs = require("fs");
-
-export default class FileSystem {
-  static create(filename: String, content: String): Boolean {
-    fs.writeFileSync(filename, content);
-    return true;
-  }
-}
