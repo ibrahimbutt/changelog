7ed99f66ca973205fb1241018e9939178be2bf0a
Ibrahim Butt <ibrahimbutt1993@gmail.com>
2019-06-26 13:31:33 +0100

    feat: get typ, scope, and date from Commit

--- src/commit.ts
+++ src/commit.ts
@@ -8,4 +8,19 @@ export default class Commit {
   public getDetails(): string {
     return this.details;
   }
+
+  public getType(): string {
+    const typeMatcherRegex = /^(\w+):/;
+    return this.details.match(typeMatcherRegex)[1];
+  }
+
+  public getScope(): string {
+    const scopeMatcherRegex = /^\w+\((\w+)\):/;
+    return this.details.match(scopeMatcherRegex)[1];
+  }
+
+  public getDate(): string {
+    const dateMatcherRegex = /(Mon|Tue|Wed|Thu|Fri|Sat|Sun+),\s(\d+\s\w{3}\s\d{4}).+/;
+    return this.details.match(dateMatcherRegex)[2];
+  }
 }
--- test/unit/commit.test.ts
+++ test/unit/commit.test.ts
@@ -1,23 +1,85 @@
 import Commit from "../../src/commit";
+const data = require("../data");
 
 describe("Commit", () => {
   describe("#getDetails", () => {
-    test("returns 'feat: device serial getter 90010eb'", () => {
-      const expected = "feat: device serial getter 90010eb";
+    test(`returns '${data.commits.bugfix.standard}'`, () => {
+      const expected = data.commits.feature.standard;
 
-      const commit = new Commit("feat: device serial getter 90010eb");
+      const commit = new Commit(data.commits.feature.standard);
       const actual = commit.getDetails();
 
       expect(actual).toEqual(expected);
     });
 
-    test("returns 'feat: device name getter and setter 90010eb'", () => {
-      const expected = "feat: device name getter and setter 90010eb";
+    test(`returns '${data.commits.bugfix.standard}'`, () => {
+      const expected = data.commits.bugfix.standard;
 
-      const commit = new Commit("feat: device name getter and setter 90010eb");
+      const commit = new Commit(data.commits.bugfix.standard);
       const actual = commit.getDetails();
 
       expect(actual).toEqual(expected);
+    });
+  });
+
+  describe("#getType", () => {
+    describe(`Given '${data.commits.feature.standard}'`, () => {
+      test(`returns 'feat'`, () => {
+        const expected = "feat";
+
+        const commit = new Commit(data.commits.feature.standard);
+        const actual = commit.getType();
+
+        expect(actual).toEqual(expected);
+      });
+    });
+
+    describe(`Given '${data.commits.bugfix.stanard}'`, () => {
+      test(`returns 'fix'`, () => {
+        const expected = "fix";
+
+        const commit = new Commit(data.commits.bugfix.standard);
+        const actual = commit.getType();
+
+        expect(actual).toEqual(expected);
+      });
+    });
+  });
+
+  describe("#getScope", () => {
+    describe(`Given '${data.commits.feature.scoped}'`, () => {
+      test(`returns 'scoped'`, () => {
+        const expected = "scoped";
+
+        const commit = new Commit(data.commits.feature.scoped);
+        const actual = commit.getScope();
+
+        expect(actual).toEqual(expected);
+      });
+    });
+
+    describe(`Given '${data.commits.bugfix.scoped}'`, () => {
+      test(`returns 'scoped'`, () => {
+        const expected = "scoped";
+
+        const commit = new Commit(data.commits.bugfix.scoped);
+        const actual = commit.getScope();
+
+        expect(actual).toEqual(expected);
+      });
+    });
+  });
+
+  describe("#getDate", () => {
+    describe(`Given '${data.commits.feature.scoped}'`, () => {
+      test(`returns '21 Jun 2019'`, () => {
+        const expected = "21 Jun 2019";
+
+        const commit = new Commit(data.commits.feature.scoped);
+        const actual = commit.getDate();
+
+        expect(actual).toEqual(expected);
+      });
     });
   });
 });
