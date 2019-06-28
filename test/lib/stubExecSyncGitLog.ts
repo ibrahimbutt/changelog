22b0aed191bb3e15cb24e837f1df76af48ad0ed4
Ibrahim Butt <ibrahimbutt1993@gmail.com>
2019-06-28 23:01:33 +0100

    test(helpers): fix formatToMatchGitLog in stubExecSyncLog
    The log (typeof string) was being split at every ',', causing major issues. Updated to use a more reliable approach.

--- test/lib/stubExecSyncGitLog.ts
+++ test/lib/stubExecSyncGitLog.ts
@@ -10,13 +10,20 @@ export default class StubExecSyncGitLog {
 
   private static prepareCommitsForBuffer(commits) {
     commits = this.objectToArray(commits);
+
     return this.formatToMatchGitLog(commits);
   }
   private static objectToArray(object) {
     return Object.values(object);
   }
 
-  private static formatToMatchGitLog(commits) {
-    return commits.toString().replace(/,/g, "\n");
+  private static formatToMatchGitLog(commits): string {
+    return commits.reduce((content, commit, index) => {
+      if (index !== commits.length - 1) {
+        return (content += commit + "\n");
+      } else {
+        return (content += commit);
+      }
+    }, "");
   }
 }
