87bbd4887f7eb6de7b8691679b1e1aace62a89db
Ibrahim <8332931+ibrahimbutt@users.noreply.github.com>
2019-12-15 14:16:09 +0000

    fix: updated paths and scripts so changelog command works  (#19)
    * chore(setup): init yarn and add main dependencies
    
    TypeScript and related dependencies so TypeScript can be used, tested and runnable in the terminal. Jest and related dependencies for testing.
    
    * chore(setup): add test script and configure jest to transform .ts files
    
    * test(ci/cd): add single test to check ci/cd intergration
    
    * chore(setup): add test:coverage script
    
    * chore(ci/cd): update travis config to run coverage script
    
    * chore(ci/cd): remove non-existent script from travis config
    
    * docs(readme): titlecase README.md title
    
    * feat: retrieve commit history formatted as <subject> <hash>
    
    On instantiation,`CommitLog` creates a `Array<Commit>`. Each `Commit` instance  stores its own details.
    
    * feat: save commit log to file
    
    * feat: changelogs have added and fixed sections
    
    * fix: categorise commit types with clashing substrings in subject
    
    * feat: changelog has version sections
    
    * chore(pre-release): v1.0.0 candidate
    
    * docs(readme): add description, usage, and examples
    
    * docs(readme): add travis and codeclimate badges
    
    * docs(readme): change example block to markdown
    
    * test(helpers): fix formatToMatchGitLog in stubExecSyncLog
    
    The log (typeof string) was being split at every ',', causing major issues. Updated to use a more reliable approach.
    
    * fix: commits are no longer excluded due to an error in parsing
    
    * chore: update .gitignore
    
    Renamed dist to lib, therefore updated .gitignore to reflect this.
    
    * chore: remove package-lock as yarn is used
    
    * chore: build to ./lib instead of ./build
    
    Build to a lib directory instead of build, per NPM package convention.
    
    * chore: add prepublishOnly script
    
    The prepublishOnly script runs before publishing your package to NPM. In this case, it'll run the Typescript compiler.

--- test/lib/stubExecSyncGitLog.ts
+++ test/lib/stubExecSyncGitLog.ts
@@ -3,12 +3,14 @@ const child_process = require("child_process");
 export default class StubExecSyncGitLog {
   static stubCommits(commits) {
     commits = this.prepareCommitsForBuffer(commits);
+
     const buffer = Buffer.from(commits);
     child_process.execSync = jest.fn(() => buffer);
   }
 
   private static prepareCommitsForBuffer(commits) {
     commits = this.objectToArray(commits);
+
     return this.formatToMatchGitLog(commits);
   }
   private static objectToArray(object) {
